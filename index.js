247274e46bddfdbe8bb0d7f6c24afe59f6bd41bf
Lily Hoskin <lily@helixcentre.com>
2019-06-13 11:55:07 +0100

    Add validation of subset

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-const Ajv = require('ajv');
+const Ajv = require("ajv");
 const ajv = new Ajv({ useDefaults: true });
 const validate = ajv.compile({
-  "type": "object",
-  "properties": {
-    "coloursOrNumbers": {
-      "oneOf": [
+  type: "object",
+  properties: {
+    coloursOrNumbers: {
+      oneOf: [
         {
-          "title": "Colours",
-          "type": "object",
-          "additionalProperties": false,
-          "properties": {
-            "name": {
-              "title": "Color Name",
-              "type": "string",
-              "default": "Red"
+          title: "Colours",
+          type: "object",
+          additionalProperties: false,
+          properties: {
+            name: {
+              title: "Color Name",
+              type: "string",
+              default: "Red"
             }
           }
         },
         {
-          "title": "Numbers",
-          "type": "object",
-          "additionalProperties": false,
-          "properties": {
-            "animal": {
-              "title": "animal",
-              "type": "string"
+          title: "Numbers",
+          type: "object",
+          additionalProperties: false,
+          properties: {
+            animal: {
+              title: "animal",
+              type: "string"
             }
           }
         }
@@ -33,32 +33,55 @@ const validate = ajv.compile({
   }
 });
 
-const data1 = {
-  'coloursOrNumbers': {
+const validateSubset = ajv.compile({
+  title: "Colours",
+  type: "object",
+  additionalProperties: false,
+  properties: {
+    name: {
+      title: "Color Name",
+      type: "string",
+      default: "Red"
+    }
   }
-}
+});
 
-const data2 = {}
+const data1 = {
+  coloursOrNumbers: {}
+};
+
+const dataEmptyObj = {};
 
 const valid = validate(data1);
-
+console.log("Data 1");
 if (!valid) {
-  console.log("Data 1")
-  console.log(data1)
-  console.log("errors")
+  console.log(data1);
+  console.log("errors");
   console.log(validate.errors);
 } else {
-  console.log(data1)
-  console.log("Is Valid")
+  console.log(data1);
+  console.log("Is Valid");
 }
 
-const valid2 = validate(data2);
+const valid2 = validate(dataEmptyObj);
+console.log("Data 2");
 if (!valid2) {
-  console.log("Data 2")
-  console.log(data2)
-  console.log("errors")
+  console.log(dataEmptyObj);
+  console.log("errors");
   console.log(validate.errors);
 } else {
-  console.log(data2)
-  console.log("Is Valid")
+  console.log(dataEmptyObj);
+  console.log("Is Valid");
+}
+
+
+const valid3 = validateSubset(dataEmptyObj)
+console.log("Data 3");
+if (!valid3) {
+  console.log(dataEmptyObj);
+  console.log("errors");
+  console.log(validate.errors);
+} else {
+  console.log(dataEmptyObj);
+  console.log("Is Valid");
 }
